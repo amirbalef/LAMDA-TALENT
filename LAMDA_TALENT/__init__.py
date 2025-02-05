618dba22946bd01e743b9b02d54e9fa2acef9a4c
Amir Balef <amirbalef@gmail.com>
2025-02-05 17:53:23 +0100

    [add] steup.py

--- LAMDA_TALENT/__init__.py
+++ LAMDA_TALENT/__init__.py
@@ -1,7 +1,7 @@
 """
-LAMDA_TALENT: A machine learning library for tabular data.
+lamda_talent: A machine learning library for tabular data.
 """
 
 __version__ = "1.0.0"
 
-from .LAMDA_TALENT import lamda_talent
+from .lamda_talent import lamda_talent
